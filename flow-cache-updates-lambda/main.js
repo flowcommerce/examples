5f8deb6a303b6ce3620f8fb444011c8554335721
Eric Mittelhammer <em@flow.io>
2017-08-11 12:05:12 -0400

    ensure callback is only called once in the error case (with test) idiomatic js edits

--- flow-cache-updates-lambda/main.js
+++ flow-cache-updates-lambda/main.js
@@ -9,11 +9,11 @@ exports.handler = function (event, context, callback) {
     const redisClient = redis.createClient({
         host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT,
-        retry_strategy: function (options) {
-            if (options.error) {
+        retry_strategy: function ({ error, total_retry_time }) {
+            if (error) {
                 return options.error;
             }
-            if (options.total_retry_time > 5000) {
+            if (total_retry_time > 5000) {
                 return new Error(`Unable to reach ${process.env.REDIS_HOST} after 5000ms`);
             }
             return 500;
@@ -23,7 +23,7 @@ exports.handler = function (event, context, callback) {
 
     // retreive a redis command for each Kinesis record based on the
     // provided event discriminator
-    const commands = event.Records.map(function (record) {
+    const commands = event.Records.map((record) => {
         const data = new Buffer(record.kinesis.data, 'base64').toString('utf8');
         const obj = JSON.parse(data);
         return cacheOps(obj.discriminator, obj);
@@ -33,14 +33,16 @@ exports.handler = function (event, context, callback) {
 
     // execute all of the commands in batch mode
     // logging the results to CloudWatch Logs
-    batch.exec(function (err, replies) {
+    batch.exec((err, replies) => {
         if (err) {
             console.log('Error when trying to cache items', err, replies);
             redisClient.quit();
             callback(err);
+            return;
         }
         console.log(`successfully updated ${replies.length} items`, replies);
         redisClient.quit();
         callback(null, replies);
+        return;
     })
 };
--- flow-cache-updates-lambda/tests/main.js
+++ flow-cache-updates-lambda/tests/main.js
@@ -5,10 +5,15 @@ const mainFixtures = require('./mainFixtures');
 const main = require('../main');
 
 describe('the main handler', function() {
+
+    afterEach(function() {
+        redis.createClient.restore();
+    });
+
     it('should insert 2 records and delete 1', function() {
         const replies = ['OK', 'OK', 1];
         
-        const exec = function(fn) {
+        const exec = (fn) => {
             fn(null, replies);
         }
 
@@ -29,6 +34,33 @@ describe('the main handler', function() {
 
         sinon.assert.calledWith(client.batch, mainFixtures.commands);
         sinon.assert.calledWith(callback, null, replies);
+        sinon.assert.calledOnce(callback);
+        sinon.assert.calledOnce(client.quit);
+    });
+
+    it('should handle batch update errors', function() {
+
+        const exec = (fn) => {
+            fn(new Error());
+        }
+
+        const client = {
+            batch: sinon.stub()
+                .returns({
+                    exec: exec
+                }),
+            quit: sinon.stub()
+        }
+
+        sinon.stub(redis, 'createClient')
+            .returns(client);
+
+        const callback = sinon.stub();
+
+        main.handler({ Records: [] }, null, callback);
+
+        sinon.assert.calledOnce(callback);
+        sinon.assert.calledWith(callback, sinon.match.instanceOf(Error));
         sinon.assert.calledOnce(client.quit);
     });
 });
